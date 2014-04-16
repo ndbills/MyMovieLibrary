b0605b6fc57687d987c2eaef731b4e77f8438eb4
Brian Wood <legion.the.unforgiven@gmail.com>
2014-04-16 11:09:05 -0600

    Renamed Model folder added createUser function in User Model

--- project/model/User.py
+++ project/model/User.py
@@ -22,6 +22,14 @@ class User(db.Document):
 		return self
 
 	@staticmethod
+	def createUser(email, password):
+		user = User(email=email)
+		user.salt = str(random.getrandbits(128))
+		user.password = hashlib.sha224(user.salt.join(password)).hexdigest()
+		user.save()
+		return user
+
+	@staticmethod
 	def validateUser(email, password):
 		user = User.objects(email=email).first()
 		if user == None:
