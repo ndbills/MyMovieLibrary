64459a64f94615f29283d8e2d054ab0ce4bb70eb
Brian Wood <legion.the.unforgiven@gmail.com>
2014-04-19 17:35:06 -0600

    Added security decoration to init.py

--- project/__init__.py
+++ project/__init__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 __version__ = '0.1'
-from flask import Flask
+from flask import Flask, session, redirect, url_for
 from flask_debugtoolbar import DebugToolbarExtension
 from flask.ext.mongoengine import MongoEngine
 
@@ -14,4 +14,24 @@ toolbar = DebugToolbarExtension(app)
 db = MongoEngine(app)
 app.debug = True
 # toolbar = DebugToolbarExtension(app)
+def security(role):
+	def wrapper(func):
+		@wraps(f)
+		def security_check(*args,**kwargs):
+			if isinstance(role, list):
+				if len(role) == 0:
+					return func(*args,**kwargs);
+				elif 'user' not in session:
+					return redirect(url_for('login', next=request.url))	
+				for r in role:
+					if r in session['user'].roles:
+						return func(*args,**kwargs); 	
+				return redirect(url_for('login', next=request.url))	
+						
+			if 'user' not in session or role not in session['user'].roles:
+				return redirect(url_for('login', next=request.url))	
+		return security_check
+	return wrapper			
+	
+
 from project.controllers import *
