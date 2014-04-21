f24eab1895102af7ee7106bef12df1962591b926
Brian Wood <legion.the.unforgiven@gmail.com>
2014-04-20 23:07:58 -0600

    Added Loan model

---
+++ project/model/Loan.py
@@ -0,0 +1,28 @@
+from project import db
+import datetime
+from User import User
+from Movie import Movie
+
+class Loan(db.Document):
+	user = db.ReferenceField(User)
+	movie = db.ReferenceField(Movie, unique_with=['user'])
+	lent_date = db.DateTimeField(default=datetime.datetime.now, required=True)
+	expected_return_date = db.DateTimeField(default=datetime.datetime.now, required=True)
+	borrower_email = db.StringField()
+
+	@staticmethod
+	def create(user,movie,email,expected_return_date=None):
+		info = Loan(user=user,movie=movie,email=email)
+		if expected_return_date:
+			info.expected_return_date = expected_return_date
+		else:
+			info.expected_return_date = info.expected_return_date + datetime.timedelta(days=7)
+		info.save()
+		return info
+
+
+	def __str__(self):
+		return "%s due %s" % (self.movie.title, self.expected_return_date.isoformat())
+
+	def __repr__(self):
+		return self.__str__()
